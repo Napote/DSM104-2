6039027cafa7475c6fe3ab8310e326250961a2da
Napote <palenciabnathaly@gmail.com>
2021-04-03 13:17:49 -0600

    Se agregaron los campos adicionales genero, fecha nacimiento, peso y altura

---
+++ complementario08/app/src/main/java/sv/edu/udb/guia08app/PersonasActivity.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.guia08app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personas);
+    }
+}
